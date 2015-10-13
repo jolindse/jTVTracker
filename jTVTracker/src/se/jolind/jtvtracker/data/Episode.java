e6439faf40e4f5eeab73f7259d77d0783f70c9e5
Johan Lindström <jolindse@hotmail.com>
2015-10-13 20:56:40 +0200

    Start of dataclasses

---
+++ jTVTracker/src/se/jolind/jtvtracker/data/Episode.java
@@ -0,0 +1,42 @@
+package se.jolind.jtvtracker.data;
+
+import java.time.ZonedDateTime;
+
+public class Episode {
+
+	private int number;
+	private String recap;
+	private ZonedDateTime epDate;
+	
+	public Episode(int number, String recap, ZonedDateTime epDate){
+		this.number = number;
+		this.recap = recap;
+		this.epDate = epDate;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getRecap() {
+		return recap;
+	}
+
+	public void setRecap(String recap) {
+		this.recap = recap;
+	}
+
+	public ZonedDateTime getEpDate() {
+		return epDate;
+	}
+
+	public void setEpDate(ZonedDateTime epDate) {
+		this.epDate = epDate;
+	}
+
+	
+}
--- jTVTracker/src/se/jolind/jtvtracker/data/Season.java
+++ jTVTracker/src/se/jolind/jtvtracker/data/Season.java
@@ -1,10 +1,53 @@
 package se.jolind.jtvtracker.data;
 
-import java.time.*;
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Season {
 
 	private int seasonNumber, numberOfEpisodes;
-	private  seasonStartDate;
+	private LocalDate seasonStartDate;
+	private List<Episode> episodeList;
+	
+	public Season (){
+	seasonNumber = 0;
+	numberOfEpisodes = 0;
+	seasonStartDate.now();
+	episodeList = new ArrayList<>();
+	}
+
+	public int getSeasonNumber() {
+		return seasonNumber;
+	}
+
+	public void setSeasonNumber(int seasonNumber) {
+		this.seasonNumber = seasonNumber;
+	}
+
+	public int getNumberOfEpisodes() {
+		return numberOfEpisodes;
+	}
+
+	public void setNumberOfEpisodes(int numberOfEpisodes) {
+		this.numberOfEpisodes = numberOfEpisodes;
+	}
+
+	public LocalDate getSeasonStartDate() {
+		return seasonStartDate;
+	}
+
+	public void setSeasonStartDate(LocalDate seasonStartDate) {
+		this.seasonStartDate = seasonStartDate;
+	}
+
+	public List<Episode> getEpisodeList() {
+		return episodeList;
+	}
+
+	public void setEpisodeList(List<Episode> episodeList) {
+		this.episodeList = episodeList;
+	}
+	
+	
 }
